dd6f7029846d0e05dceedade87828ab541077e4b
Romaine Allen <romallen1@gmail.com>
2021-09-28 20:34:57 +0900

    Create outline of REST APIs & added prisma

--- src/server.js
+++ src/server.js
@@ -4,22 +4,59 @@ const setupExpressServer = () => {
   const app = express();
   app.use(express.json());
 
-  app.get("/", (req, res) => {
-    res.send("ITS ALIVE!!!")
-  });
+//   app.get("/", (req, res) => {
+//     res.send("ITS ALIVE!!!")
+//   });
 
-  app.get("/category/", (req, res) => {
-    res.send("ITS ALIVE!!!")
-  });
-  app.get("/categories/", (req, res) => {
-    res.send("ITS ALIVE!!!")
-  });
-  app.get("/question", (req, res) => {
-    res.send("ITS ALIVE!!!")
-  });
-  app.get("/questions", (req, res) => {
-    res.send("ITS ALIVE!!!")
-  });
+
+    /*
+    CREATE
+    */
+    app.post("/question/", (req, res) => {
+        res.sendStatus(200);
+    });
+
+
+     /*
+    REQUEST
+    */
+    app.get("/category/", (req, res) => {
+        res.send("ITS ALIVE!!!")
+    });
+    app.get("/categories/", (req, res) => {
+        res.send("ITS ALIVE!!!")
+    });
+    app.get("/question", (req, res) => {
+        res.send("ITS ALIVE!!!")
+    });
+    app.get("/questions/", (req, res) => {
+        res.send("ITS ALIVE!!!")
+    });
+
+
+
+    /*
+    UPDATE
+    */
+    app.patch("/category/", (req, res) => {
+
+    });
+
+    app.patch("/question/", (req, res) => {
+
+    });
+
+
+     /*
+    DELETE
+    */
+    app.delete("/category", (req, res) => {
+
+    });
+
+    app.delete("/question", (req, res) => {
+
+    });
 
 
   return app;
